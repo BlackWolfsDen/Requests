8dfd3ade5ffafdf9a1da9dead0f93653b5bea101
Grumbo <blackwolfsden@hotmail.com>
2017-10-20 19:35:43 -0600

    Update Randomized_Item_Dropper.lua
    updated

--- Eluna/ToK124/Randomized_Item_Dropper.lua
+++ Eluna/ToK124/Randomized_Item_Dropper.lua
@@ -7,17 +7,16 @@
 -- © For Tok124 of EmuDevs.com -- ©
 
 local command = "#roll"; -- command used
-local roll_item_ID = 8000001; -- id of the roll item.
+local roll_item_ID = 63020; -- id of the roll item.
 local ItemName = GetItemLink(roll_item_ID); -- ingame item link.
 local cost = 1; -- cost of item per roll.
 local tries = 3; -- catch 22 in case player can only have 1 item they allready have then reroll x tries for a new item.
 local try = 1;
 
-local Item_Drops = { -- {Roll_Value_Required {item_id_1, item_id_2, item_id_3, item_id_4, item_id_5....}},
-		[1] = {10,{6400016, 6400019}}; -- 10 or less roll.
-		[2] = {50,{800000, 800001}}; -- 50 or less roll.
-		[3] = {100,{6400020, 6400021}}, -- 100 or less roll.
-		[4] = {200,{6400022, 6400023}}, -- 200 or less roll.
+local Item_Drops = { -- {chance_Value {item_id_1, item_id_2, item_id_3, item_id_4, item_id_5....}},
+		[1] = {6400020, 6400021}, -- green 60% deop rate.
+		[2] = {800000, 800001}; -- blue 30% drop chance.
+		[3] = {6400016, 6400019}; -- purple 10% drop chance.
 	};
 
 local time = tonumber(os.time());
@@ -29,40 +28,24 @@ local function ItemRoll(event, player, msg, Type, lang)
 	
 		local count = player:GetItemCount(roll_item_ID);
 		
-		if(player:HasItem(roll_item_ID, cost) == false)then	player:SendBroadcastMessage("You need "..cost.." of "..ItemName..". You have "..count..".");
+			if(player:HasItem(roll_item_ID, cost) == false)then	player:SendBroadcastMessage("You need "..cost.." of "..ItemName..". You have "..count..".");
 
-		else
-		
-			local roll = math.random(1, Item_Drops[#Item_Drops][1]);
-			local chance = nil;
-			local comp = 0;
-			
-				for comp = 1, #Item_Drops do
-			
-					if(roll <= Item_Drops[comp][1])then
-					
-						chance = comp;
-					end
-				end
+			else
 
-				if(chance == nil)then ItemRoll(event, player, msg, Type, lang);
+				local roll = math.random(1, #Item_Drops);
+				local item = math.random(1, #Item_Drops[roll]);
 
-				else
-			
-					local item = math.random(1, #Item_Drops[chance][2]);
-					
-						if(player:AddItem(Item_Drops[chance][2][item], 1))then player:RemoveItem(roll_item_ID, cost);
-							
+					if(player:AddItem(Item_Drops[roll][item], 1))then player:RemoveItem(roll_item_ID, cost);
+
+					else 
+						if(try >= tries)then player:SendBroadcastMessage("There was an error trying to gift you an item. please try again later.");
+
 						else 
-							if(try >= tries)then player:SendBroadcastMessage("There was an error trying to gift you an item. please try again later.");
-							
-							else 
-								try = try + 1;
-								ItemRoll(event, player, msg, Type, lang);
-							end
+							try = try + 1;
+							ItemRoll(event, player, msg, Type, lang);
 						end
-				end
-		end
+					end
+			end
 	return false;
 	end
 end
